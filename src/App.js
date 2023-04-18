256d354960e3b2b2f70c5b08273e521b776d4953
Kai M <106705565+KaiMHK@users.noreply.github.com>
2023-04-18 01:10:17 -0700

    add

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div id="construct">Haokai Mao Website Constructing</div>
+      <div id="construct">Haokai Mao Website Constructing, Hello!</div>
     </div>
   );
 }
