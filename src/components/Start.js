2f1f898b58f9144cd993be883e349c753c26f3c3
Kai M <106705565+KaiMHK@users.noreply.github.com>
2023-04-24 22:46:58 -0700

    change-To-to

--- src/components/Start.js
+++ src/components/Start.js
@@ -4,7 +4,7 @@ import "../styles/Start.css"
 export default function Start(params) {
   return (
   <div>
-    <h1 id="title">Welcome To Kai's Protfolio</h1>
+    <h1 id="title">Welcome to Kai's Protfolio</h1>
     <Link to="main" className="custom-btn btn-5" id="mainBtn"><div id="buttonText">Main Page</div></Link>
   </div>
   );
