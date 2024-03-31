1fe0ec6a614cbd1f31b7f1fdcdf93fbfa4d05894
Kai M <106705565+KaiMHK@users.noreply.github.com>
2024-03-30 21:20:28 -0500

    update

--- src/components/ImgDisplayer.js
+++ src/components/ImgDisplayer.js
@@ -1,12 +1,8 @@
 // import resumeImage from '../assets/ResumeSDE424.png';
-import resumeSDE from "../assets/ResumeSDE424.jpg";
+// import resumeSDE from "../assets/ResumeSDE424.jpg";
 
 import "../styles/ImgDisplayer.css";
 
 export default function PdfDisplay(params) {
-    return (
-        <div id="resumePage">
-            <img id="resume" src={resumeSDE}/>
-        </div>
-    );
-};
+  return <div id="resumePage">{/* <img id="resume" src={resumeSDE}/> */}</div>;
+}
